9ffc7967af3eab698d555faaae673302fc254849
zabefofoon <zabefofoon@naver.com>
2026-06-25 14:46:39 +0900

    feat: paraglide 세팅

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from 'wxt';
-import tailwindcss from '@tailwindcss/vite';
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
-  manifest: {
-    permissions: ['contextMenus', 'tabs', 'storage'],
-  },
-  vite: () => ({
-    plugins: [tailwindcss()],
-  }),
+	srcDir: 'src',
+	modules: ['@wxt-dev/module-svelte'],
+	manifest: {
+		permissions: ['contextMenus', 'tabs', 'storage']
+	},
+	vite: () => ({
+		plugins: [
+			paraglideVitePlugin({
+				project: './project.inlang',
+				outdir: './src/lib/paraglide',
+				disableAsyncLocalStorage: true,
+				strategy: ['preferredLanguage']
+			}),
+			tailwindcss()
+		]
+	}),
 
-  // TODO 개발용
-  webExt: {
-    chromiumArgs: [
-      `--user-data-dir=${process.env.HOME}/.wxt-chrome-profile`,
-      '--disable-blink-features=AutomationControlled',
-      '--exclude-switches=enable-automation',
-    ],
-  },
-});
+	// TODO 개발용
+	webExt: {
+		chromiumArgs: [
+			`--user-data-dir=${process.env.HOME}/.wxt-chrome-profile`,
+			'--disable-blink-features=AutomationControlled',
+			'--exclude-switches=enable-automation'
+		]
+	}
+})
